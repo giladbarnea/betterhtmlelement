7707f919fc6e11c33275724d72179956369a21ba
Gilad Barnea <giladbrn@gmail.com>
2019-07-08 19:27:57 +0300

    extend function cleaner

--- dist/all.js
+++ dist/all.js
@@ -1,36 +1,3 @@
-/*function extend(sup, base) {
-    var descriptor = Object.getOwnPropertyDescriptor(
-        base.prototype, 'constructor'
-    );
-    base.prototype = Object.create(sup.prototype);
-    var handler = {
-        construct: function (target, args) {
-            var obj = Object.create(base.prototype);
-            this.apply(target, obj, args);
-            return obj;
-        },
-        apply: function (target, that, args) {
-            sup.apply(that, args);
-            base.apply(that, args);
-        }
-    };
-    var proxy = new Proxy(base, handler);
-    descriptor.value = proxy;
-    Object.defineProperty(base.prototype, 'constructor', descriptor);
-    return proxy;
-}
-
-// var Person = function (name) {
-//     this.name = name;
-// };
-
-var Boy = extend(Person, function (name, age) {
-    this.age = age;
-});
-var Peter = new Boy('Peter', 13);
-
-// Boy.prototype.gender = 'M';
-*/
 class BetterHTMLElement {
     /*[Symbol.toPrimitive](hint) {
         console.log('from toPrimitive, hint: ', hint, '\nthis: ', this);
--- src/index.ts
+++ src/index.ts
@@ -397,39 +397,7 @@ interface AnimateOptions {
     timingFunction?: AnimationTimingFunction;
 }
 
-/*function extend(sup, base) {
-    var descriptor = Object.getOwnPropertyDescriptor(
-        base.prototype, 'constructor'
-    );
-    base.prototype = Object.create(sup.prototype);
-    var handler = {
-        construct: function (target, args) {
-            var obj = Object.create(base.prototype);
-            this.apply(target, obj, args);
-            return obj;
-        },
-        apply: function (target, that, args) {
-            sup.apply(that, args);
-            base.apply(that, args);
-        }
-    };
-    var proxy = new Proxy(base, handler);
-    descriptor.value = proxy;
-    Object.defineProperty(base.prototype, 'constructor', descriptor);
-    return proxy;
-}
 
-// var Person = function (name) {
-//     this.name = name;
-// };
-
-var Boy = extend(Person, function (name, age) {
-    this.age = age;
-});
-var Peter = new Boy('Peter', 13);
-
-// Boy.prototype.gender = 'M';
-*/
 
 
 class BetterHTMLElement {
