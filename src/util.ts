b3caf12c6719f2822b8dcb42a624f4893a767753
giladbarnea <giladbrn@gmail.com>
2019-10-15 10:33:20 +0300

    enumerate handles undefined and null

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,12 @@
+function enumerate(obj: undefined): [void];
+function enumerate(obj: null): never;
 function enumerate<T>(obj: T[]): [number, T][];
 function enumerate<T>(obj: T): [keyof T, T[keyof T]][];
 function enumerate(obj) {
+    if (obj === undefined)
+        return [];
+    if (obj === null)
+        throw new TypeError('null is not iterable');
     let array = [];
     if (Array.isArray(obj) || typeof obj[Symbol.iterator] === 'function') {
         let i: number = 0;
